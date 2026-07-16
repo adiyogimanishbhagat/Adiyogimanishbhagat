3e7b085d6d56436f43842e1d8cf9c0a420ef9e79
adiyogimanishbhagat <mkbgat@gmail.com>
2026-07-16 22:36:18 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+{
+  "short_name": "Adiyogi",
+  "name": "Adiyogi Manish Bhagat Premium Wellness",
+  "icons": [
+    {
+      "src": "assets/icon-192.png",
+      "type": "image/png",
+      "sizes": "192x192"
+    },
+    {
+      "src": "assets/icon-512.png",
+      "type": "image/png",
+      "sizes": "512x512"
+    }
+  ],
+  "start_url": "/index.html",
+  "background_color": "#081c15",
+  "theme_color": "#1b4332",
+  "display": "standalone",
+  "orientation": "portrait"
+}
